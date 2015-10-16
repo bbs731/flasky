8f5597a9b09337e62bac68f963f52062245564a7
chunlei <chunlz@cn.ibm.com>
2015-10-16 21:45:06 +0800

    Chapter 4c adding flash support

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
@@ -14,18 +14,19 @@ manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
 
-    
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
 
 @app.errorhandler(500)
-def interanl_server_error(e):
+def internal_server_error(e):
     return render_template('500.html'), 500
 
 
@@ -33,6 +34,9 @@ def interanl_server_error(e):
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
         session['name'] = form.name.data
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'))
@@ -40,4 +44,3 @@ def index():
 
 if __name__ == '__main__':
     manager.run()
-

df11d60e75284a5ca91cd92a5cf0da9fcedc67b0
chunlei <chunlz@cn.ibm.com>
2015-10-16 10:54:31 +0800

    Chapter 2c adding flask-script manager

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from flask.ext.script import Manager
+
 app = Flask(__name__)
+
+manager = Manager(app)
+
 
 @app.route('/')
 def index():
@@ -10,5 +15,5 @@ def user(name):
     return '<h1>Hello, %s!</h1>' % name
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
 

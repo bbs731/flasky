f5262e299c8ec83f9385289f30b6a94f552fc967
chunlei <chunlz@cn.ibm.com>
2015-10-16 20:32:35 +0800

    Chapter 4b same the form posted data into session variable

--- hello.py
+++ hello.py
@@ -30,12 +30,11 @@ def interanl_server_error(e):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
 if __name__ == '__main__':

ad4e3b94476196a6dcbfbbd9dae351ca612a1ce5
chunlei <chunlz@cn.ibm.com>
2015-10-17 18:16:56 +0800

    Chapter 8d bug fix UserMixin db

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,6 +2,7 @@ from flask import render_template, redirect, request, url_for, flash
 from flask.ext.login import login_user, logout_user, login_required, \
      current_user
 from . import auth
+from .. import db
 from ..models import User
 from .forms import LoginForm, RegistrationForm
 from ..email import send_email
--- app/models.py
+++ app/models.py
@@ -12,7 +12,7 @@ class Role(db.Model):
     def __repr__(self):
         return '<Role %r>' % self.name
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64), unique=True, index=True)

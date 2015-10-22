2b91492ba2eca5dadb9cb9e322ca5451185242df
chunlei <chunlz@cn.ibm.com>
2015-10-22 13:56:59 +0800

    Chapter 10a adding user profile and last_seen and memeber_since fields

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -117,4 +117,22 @@ class UserModelTestCase(unittest.TestCase):
         u = AnonymousUser()
         self.assertFalse(u.can(Permission.FOLLOW))
 
+    def test_timestamps(self):
+        u = User(password='cat')
+        db.session.add(u)
+        db.session.commit()
+        self.assertTrue(
+            (datetime.utcnow() - u.member_since).total_seconds() < 3)
+        self.assertTrue(
+            (datetime.utcnow() - u.last_seen).total_seconds() < 3)
+
+    def test_ping(self):
+        u = User(password='cat')
+        db.session.add(u)
+        db.session.commit()
+        time.sleep(2)
+        last_seen_before = u.last_seen
+        u.ping()
+        self.assertTrue(u.last_seen > last_seen_before)
+
         
